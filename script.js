feda3cb6909e22609821b0467e152426844a454a
Oleh Kovalchuk <rod07@ukr.net>
2022-04-06 23:59:21 +0300

    burger menu no hover

---
+++ script.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector(".header_burger")
+let nav = document.querySelector(".nav_menu")
+let logo = document.querySelector(".header")
+
+burger.addEventListener("click", () => {
+    nav.style.display = "block"
+})
